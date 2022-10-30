c6a02a6775261f65ee9d05fcae21b4dd0cb95bf3
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-10-30 22:08:19 +0100

    Components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,23 @@
-export const App = () => {
-  return (
+import { Component } from "react";
+import Searchbar from '../Searchbar/Searchbar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import Loader from '../Loader/Loader';
+import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
+
+export default class App extends Component {
+  render() {
+    return (
     <div>
       <Searchbar />
-      <ImageGallery />
-      <ImageGalleryItem />
+      <ImageGallery>
+        <ImageGalleryItem />
+      </ImageGallery>
       <Loader />
       <Button />
       <Modal />
     </div>
   );
+  } 
 };
